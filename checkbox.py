f282c64b26fcd93fe45726544fd8c48f759bac04
syida73 <51057921+syida73@users.noreply.github.com>
2022-08-24 10:56:28 +0800

    Add files via upload

---
+++ checkbox.py
@@ -0,0 +1,13 @@
+%%writefile myfirstapp.py
+import streamlit as st
+import pandas as pd
+
+st.header("My first Streamlit App")
+
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))

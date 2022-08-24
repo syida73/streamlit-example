05068a369fb63ac7d6ccd755d67b7ff7795cbef5
syida73 <51057921+syida73@users.noreply.github.com>
2022-08-24 13:59:07 +0800

    Update image.py

--- image.py
+++ image.py
@@ -1,17 +1,9 @@
-#importing streamlit library
-
+%%writefile myfirstapp.py
 import streamlit as st
-
-from PIL import Image
-
-
-
-#opening the image
+import numpy as np
+import pandas as pd
 
 image = Image.open('https://bouqs.com/blog/wp-content/uploads/2021/11/iris-flower-meaning-and-symbolism.jpg')
 
-
-
-#displaying the image on streamlit app
 
 st.image(image, caption='Sunset view')

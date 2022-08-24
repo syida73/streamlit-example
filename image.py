2df852881d01f071dfb74d62ea84f89d12cd3cdd
syida73 <51057921+syida73@users.noreply.github.com>
2022-08-24 14:02:13 +0800

    Delete image.py

--- image.py
+++
@@ -1,9 +0,0 @@
-%%writefile myfirstapp.py
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-image = Image.open('https://bouqs.com/blog/wp-content/uploads/2021/11/iris-flower-meaning-and-symbolism.jpg')
-
-
-st.image(image, caption='Sunset view')
